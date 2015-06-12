f9fdf016d448a42d4efbeabb548037bc2028ac5a
Karen Santana <karensantana.g@hotmail.com>
2015-06-12 09:52:16 -0400

    Erase markdown controller

--- App_Plugins/MarkDownEditor/markdowneditor.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module("umbraco")
-    .controller("My.MarkdownEditorController", 
-    function ($scope, notificationsService) {
-        notificationsService.error("Error", "Digite los valores correctos");          
-    });
-     
-      
